06ea4d8b545636368e548c4e3e318db2bd10e7c0
Ashley Wulber <ashley@system76.com>
2023-11-16 19:54:43 -0500

    chore: update libcosmic

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,14 @@ mod localize;
 use std::{collections::HashMap, path::Path};
 
 use cosmic::{
-    executor,
+    app::{Command, Core, Settings},
     iced::{
-        theme,
         widget::{column, text},
-        window, Application, Color, Command,
+        Color,
     },
-    iced_sctk::{
-        commands::layer_surface::{self, Anchor, Layer},
-        settings::InitialSurface,
-    },
-    iced_style::{application, Theme},
-    settings,
+    iced_sctk::commands::layer_surface::{self, Anchor, Layer},
+    iced_style::application::{self, Appearance},
+    Theme,
 };
 use localize::localize;
 use log::info;
@@ -25,43 +21,32 @@ fn main() {
     info!("Starting ActivateLinux");
     localize();
 
-    let mut settings = settings();
-    settings.id = Some("gay.ash.ActivateLinux".to_string());
-    settings.initial_surface = InitialSurface::LayerSurface(
-        cosmic::iced::wayland::actions::layer_surface::SctkLayerSurfaceSettings {
-            layer: Layer::Overlay,
-            anchor: Anchor::BOTTOM.union(Anchor::RIGHT),
-            output: cosmic::iced::wayland::actions::layer_surface::IcedOutput::All,
-            margin: cosmic::iced::wayland::actions::layer_surface::IcedMargin {
-                top: 0,
-                right: 36,
-                bottom: 24,
-                left: 0,
-            },
-            keyboard_interactivity: layer_surface::KeyboardInteractivity::None,
-            exclusive_zone: -1,
-            pointer_interactivity: false,
-            ..Default::default()
-        },
-    );
-
-    Activate::run(settings).expect("Failed to run ActivateLinux");
+    cosmic::app::run::<Activate>(
+        Settings::default()
+            .antialiasing(true)
+            .client_decorations(false)
+            .debug(false)
+            .default_text_size(16.0)
+            .scale_factor(1.0)
+            .no_main_window(true)
+            .exit_on_close(true),
+        (),
+    )
+    .expect("Failed to run Activate Linux");
 }
 
 struct Activate {
+    core: Core,
     distro: String,
 }
 
-impl Application for Activate {
-    type Executor = executor::Default;
-
+impl cosmic::Application for Activate {
     type Message = ();
-
-    type Theme = Theme;
-
+    type Executor = cosmic::executor::single::Executor;
     type Flags = ();
+    const APP_ID: &'static str = "com.system76.CosmicLauncher";
 
-    fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
+    fn init(core: Core, _flags: Self::Flags) -> (Self, Command<()>) {
         let distro = if let Some(Ok(distro)) = Path::new("/.flatpak-info")
             .exists()
             .then(|| os_release::OsRelease::new_from(Path::new("/var/run/host/etc/os-release")))
@@ -73,53 +58,68 @@ impl Application for Activate {
             "Linux".to_string()
         };
 
-        (Self { distro }, cosmic::iced::Command::none())
+        (
+            Self { distro, core },
+            cosmic::iced_sctk::commands::layer_surface::get_layer_surface(
+                cosmic::iced::wayland::actions::layer_surface::SctkLayerSurfaceSettings {
+                    id: cosmic::iced::window::Id(1),
+                    layer: Layer::Overlay,
+                    anchor: Anchor::BOTTOM.union(Anchor::RIGHT),
+                    output: cosmic::iced::wayland::actions::layer_surface::IcedOutput::All,
+                    margin: cosmic::iced::wayland::actions::layer_surface::IcedMargin {
+                        top: 0,
+                        right: 36,
+                        bottom: 24,
+                        left: 0,
+                    },
+                    keyboard_interactivity: layer_surface::KeyboardInteractivity::None,
+                    exclusive_zone: -1,
+                    pointer_interactivity: false,
+                    ..Default::default()
+                },
+            ),
+        )
     }
 
-    fn title(&self) -> String {
-        let distro_var = HashMap::from([("distro", self.distro.clone())]);
-        fl!("activate", distro_var)
+    fn core(&self) -> &Core {
+        &self.core
     }
 
-    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
-        cosmic::iced::Command::none()
+    fn core_mut(&mut self) -> &mut Core {
+        &mut self.core
     }
 
-    fn view(
-        &self,
-        _id: window::Id,
-    ) -> cosmic::iced::Element<'_, Self::Message, cosmic::iced::Renderer<Self::Theme>> {
+    fn style(&self) -> Option<<Theme as application::StyleSheet>::Style> {
+        Some(<Theme as application::StyleSheet>::Style::Custom(Box::new(
+            |theme| {
+                let mut text = theme.cosmic().on_bg_color();
+                text.alpha = 0.5;
+                let text = text.into();
+
+                Appearance {
+                    background_color: Color::from_rgba(0.0, 0.0, 0.0, 0.0),
+                    text_color: text,
+                    icon_color: text,
+                }
+            },
+        )))
+    }
+
+    fn view(&self) -> cosmic::Element<Self::Message> {
+        unimplemented!()
+    }
+
+    fn view_window(&self, _id: cosmic::iced::window::Id) -> cosmic::Element<Self::Message> {
         let distro_var = HashMap::from([("distro", self.distro.clone())]);
         column![
-            text(fl!("activate", distro_var.clone()))
-                .size(24)
-                .style(theme::Text::Color(Color::from_rgba(1.0, 1.0, 1.0, 0.5))),
-            text(fl!("go-to-settings", distro_var))
-                .size(16)
-                .style(theme::Text::Color(Color::from_rgba(1.0, 1.0, 1.0, 0.5))),
+            text(fl!("activate", distro_var.clone())).size(24),
+            text(fl!("go-to-settings", distro_var)).size(16)
         ]
         .spacing(4)
         .into()
     }
 
-    fn close_requested(&self, _id: window::Id) -> Self::Message {
-        info!("Exiting ActivateLinux");
-        std::process::exit(0);
-    }
-
-    fn style(&self) -> <Self::Theme as application::StyleSheet>::Style {
-        <Self::Theme as application::StyleSheet>::Style::Custom(Box::new(CustomStyleSheet))
-    }
-}
-
-pub struct CustomStyleSheet;
-impl application::StyleSheet for CustomStyleSheet {
-    type Style = Theme;
-
-    fn appearance(&self, _style: &Self::Style) -> application::Appearance {
-        application::Appearance {
-            background_color: Color::from_rgba(0.0, 0.0, 0.0, 0.0),
-            text_color: Color::from_rgba(1.0, 1.0, 1.0, 0.5),
-        }
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        cosmic::iced::Command::none()
     }
 }
